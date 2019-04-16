758034f7c2b6218e56431629db18b5b0a13f3000
isaq.smartmachine@gmail.com <isaq.smartmachine@gmail.com>
2019-04-16 16:29:43 -0300

    'mudancanoexc'

--- bloodstoneonlinebr/settings.py
+++ bloodstoneonlinebr/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt_BR'
+LANGUAGE_CODE = 'pt_br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
